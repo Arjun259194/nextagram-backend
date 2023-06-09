16b75f8d5d84e1b36ce5c33387c957393e4778bc
arjun259194 <arjun259194@gmail.com>
2023-06-09 20:15:45 +0530

    route to get letest post is ready

--- controllers/post.go
+++ controllers/post.go
@@ -5,11 +5,43 @@ import (
 
 	"github.com/Arjun259194/nextagram-backend/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (ctrl *Controllers) GetTopPosts(c *fiber.Ctx) error {
-	return c.SendStatus(fiber.StatusOK)
+
+	options := options.Find().SetLimit(10)
+	filter := bson.M{}
+
+	result, err := ctrl.DB.PostModel.Find(ctrl.DB.Ctx, filter, options)
+
+	if err != nil {
+		var (
+			status int
+			errRes types.ErrorResponse
+		)
+		if err == mongo.ErrNoDocuments {
+			status = fiber.StatusNotFound
+			errRes = types.NewErrorResponse(status, err, "No post found")
+		} else {
+			status = fiber.StatusInternalServerError
+			errRes = types.NewErrorResponse(status, err, "Error while fetching from database")
+		}
+		return c.Status(status).JSON(errRes)
+	}
+
+	var posts []types.Post
+	if err := result.All(ctrl.DB.Ctx, &posts); err != nil {
+		errRes := types.NewErrorResponse(fiber.StatusInternalServerError, err, "Error while decoding data")
+		return c.Status(fiber.StatusInternalServerError).JSON(errRes)
+	}
+
+	res := types.NewSuccessResponse(fiber.StatusOK, posts, "top 10 latest posts")
+	return c.Status(fiber.StatusOK).JSON(res)
+
 }
 
 func (ctrl *Controllers) PostCreatePost(c *fiber.Ctx) error {
@@ -17,7 +49,7 @@ func (ctrl *Controllers) PostCreatePost(c *fiber.Ctx) error {
 
 	file, err := c.FormFile("file")
 	if err != nil {
-		errRes := types.NewErrorResponse(fiber.StatusBadRequest, err, "Can't get file from formdata")
+		errRes := types.NewErrorResponse(fiber.StatusBadRequest, err, "Can't get file from formData")
 		return c.Status(fiber.StatusBadRequest).JSON(errRes)
 	}
 
